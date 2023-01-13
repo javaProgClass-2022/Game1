00b061baa9cf8af19a3f66559397cd7f727000a8
Hetul3 <97708864+Hetul3@users.noreply.github.com>
2023-01-13 12:15:39 -0500

    Add files via upload

---
+++ src/game/Lawnmower.java
@@ -0,0 +1,12 @@
+package game;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public class Lawnmower extends Rectangle {
+	
+	static int width, height, speed;
+	int x = 50, y;
+	boolean triggered = false;
+	BufferedImage img;
+	
+}
